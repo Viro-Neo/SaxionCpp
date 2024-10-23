ba0c7224cb3e0aa9b2b1fff6cca365bf3773d231
Van-Do <van-do.pham@epitech.eu>
2024-10-23 17:41:22 +0200

    :white_check_mark: added simple test with main function

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,15 @@
-int main(void)
+#include "Character.hpp"
+
+int main()
 {
+    Character character(5);
+
+    character.addInventoryItem("Sword");
+    character.addInventoryItem("Shield");
+    character.addInventoryItem("Potion");
+    character.addInventoryItem("Bow");
+    character.addInventoryItem("Arrow");
+    character.addInventoryItem("Dagger");
+    character.printInventory();
     return 0;
 }
