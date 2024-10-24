dda9ce46d0e2279140625b8f7be20ee6b678c914
Van-Do <van-do.pham@epitech.eu>
2024-10-24 15:03:34 +0200

    :sparkles: updated main to randomize the total points of the enemies

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,33 @@
-#include "Character.hpp"
+#include "Player.hpp"
+#include "Enemy.hpp"
+#include <random>
+#include <algorithm>
 
-void inventoryTest()
-{
-    Character character(5);
+// Randomize total points for each enemy, the last enemy will have the highest total points
+void randomizeTotalPoints(std::shared_ptr<Enemy[]> enemies, int nbrEnemies) {
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> dis(40, 120);
+    std::vector<int> PtsToDistribute;
 
-    character.addInventoryItem("Sword");
-    character.addInventoryItem("Shield");
-    character.addInventoryItem("Potion");
-    character.addInventoryItem("Bow");
-    character.addInventoryItem("Arrow");
-    character.addInventoryItem("Dagger");
-    character.printInventory();
+    for (int i = 0; i < nbrEnemies; i++)
+        PtsToDistribute.push_back(dis(gen));
+    std::sort(PtsToDistribute.begin(), PtsToDistribute.end());
+    for (int i = 0; i < nbrEnemies; i++) {
+        enemies[i].setTotalPoints(PtsToDistribute[i]);
+    }
 }
 
 int main()
 {
-    Character player(5);
+    int nbrEnemies = 5;
+    Player player(5);
+    std::shared_ptr<Enemy[]> enemies = std::shared_ptr<Enemy[]>(new Enemy[nbrEnemies]);
+
+    randomizeTotalPoints(enemies, nbrEnemies);
+
+    player.displayStats();
+    for (int i = 0; i < 5; i++)
+        enemies[i].displayStats();
     return 0;
 }
