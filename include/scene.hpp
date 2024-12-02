3c9901bef1c72a38f48e061305954748d028b4b5
Viro-Neo <van-do.pham@epitech.eu>
2024-12-02 17:27:20 +0100

    :sparkles: update scene class

--- include/scene.hpp
+++ include/scene.hpp
@@ -6,18 +6,18 @@
 class Scene {
 public:
     explicit Scene(std::string identifier);
-    ~Scene() = default;
-
-    void update();
-    void render(sf::RenderWindow& window);
+    virtual ~Scene() = default;
 
     void addGameObject(GameObject& object);
+    virtual void handleEvent(const sf::Event& event, sf::RenderWindow& window);
+    virtual void update();
+    virtual void render(sf::RenderWindow& window);
+
     [[nodiscard]] std::string getIdentifier() const;
 
 private:
     const std::string identifier;
     std::vector<GameObject*> listOfGameObjects;
-
 };
 
 #endif //SCENE_HPP
