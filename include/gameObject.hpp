1ecb674905d3f03d1c7d8de31cfbd8781e081933
Viro-Neo <van-do.pham@epitech.eu>
2024-12-02 17:27:05 +0100

    :sparkles: update gameObject class

--- include/gameObject.hpp
+++ include/gameObject.hpp
@@ -7,12 +7,13 @@ class GameObject {
 public:
     explicit GameObject(std::string identifier);
     GameObject(const GameObject& other);
+
     virtual ~GameObject() = default;
 
-    virtual void handleEvent(const sf::Event& event, sf::RenderWindow& window) { }
+    virtual void handleEvent(const sf::Event& event, sf::RenderWindow& window) { };
     virtual void update() = 0;
     virtual void render(sf::RenderWindow& window) = 0;
-    [[nodiscard]] std::string getIdentifier() const { return this->identifier; }
+    [[nodiscard]] std::string getIdentifier() const;
 
 private:
     const std::string identifier;
