7eb28b075d185f3525c89bf58ae4c6a6c0729f55
Viro-Neo <van-do.pham@epitech.eu>
2024-12-18 18:01:14 +0100

    :mute: remove console output

--- src/button.cpp
+++ src/button.cpp
@@ -14,12 +14,6 @@ Button::Button(const std::string &identifier, sf::Font& font, const std::string&
     this->text.setString(buttonText);
     this->text.setCharacterSize(26);
     this->text.setFillColor(sf::Color::White);
-    // const sf::FloatRect textRect = this->text.getLocalBounds();
-    // std::cout << "textRect.width: " << textRect.width << std::endl;
-    // std::cout << "textRect.height: " << textRect.height << std::endl;
-    // this->text.setOrigin(0, 0);
-
-    this->setPosition(sf::Vector2f(0.0f, 0.0f));
 }
 
 void Button::handleEvent(const sf::Event& event, sf::RenderWindow& window) {
@@ -50,8 +44,6 @@ void Button::setPosition(const sf::Vector2f position) {
     this->shape.setPosition(position);
     const sf::Vector2f size = this->shape.getSize();
     const sf::FloatRect textRect = this->text.getLocalBounds();
-    std::cout << "textRect.width: " << textRect.width << std::endl;
-    std::cout << "textRect.height: " << textRect.height << std::endl;
     this->text.setPosition(position.x + size.x / 2.0f - textRect.width / 2.0f, position.y + size.y / 2.0f - textRect.height / 2.0f);
 }
 
